6b5979aa3342a464552bb86de9b7e6ff5f404594
Laurent Mazare <laurent.mazare@gmail.com>
2011-09-11 10:21:35 +0100

    Test content.

--- java/testAvl.java
+++ java/testAvl.java
@@ -1,15 +1,24 @@
 // This software is in the public domain, furnished "as is", without technical
 // support, and with no warranty, express or implied, as to its usefulness for
 // any purpose.
-import java.util.Random;
+import java.util.*;
 
 class TestAVL {
   static Random generator = new Random(123456);
 
   public static AVL randomTree(int nbElements) {
     AVL myTree = new AVL();
-    for(int i = 0; i < nbElements; i++)
-      myTree.add(generator.nextInt(1000));
+    List<Integer> content = new LinkedList<Integer>();
+    for(int i = 0; i < nbElements; i++) {
+      int v = generator.nextInt(1000);
+      content.add(v);
+      myTree.add(v);
+    }
+    boolean check = true;
+    for(int i: content) {
+      check = check && myTree.hasKey(i);
+    }
+    System.out.printf("Created tree with %d elements, ok: %s\n", nbElements, check);
     return myTree;
   }
 
@@ -23,7 +32,7 @@ class TestAVL {
       System.out.printf("Height:%d %s %s\n", myTree.getHeight(), myTree.checkBalanced(), myTree.checkTree());
     }
     for(int i = 0; i < 10; i++) {
-      AVL myTree = randomTree(1000);
+      AVL myTree = randomTree(500);
       System.out.printf("Height:%d %s %s\n", myTree.getHeight(), myTree.checkBalanced(), myTree.checkTree());
       }
     }
